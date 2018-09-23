f8c1ba49572704b27203bdc27af74ae764e66009
Anton Nikifarau <411F42@gmail.com>
2018-09-23 13:49:50 +0300

    init commit

---
+++ test/test_lein_project/core_test.clj
@@ -0,0 +1,7 @@
+(ns test-lein-project.core-test
+  (:require [clojure.test :refer :all]
+            [test-lein-project.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
